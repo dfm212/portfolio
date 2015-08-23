a1fe699d4e389205e9b2e8a7246ef3544eb7e97b
dfm212 <daniel_ve14@hotmail.com>
2015-08-23 13:47:25 -0430

    act 12

--- js/app.js
+++ js/app.js
@@ -2,4 +2,84 @@ var app = angular.module("portgolio", []);
 
 app.controller('', function(){
 
+
+
+var boardRow =
+[
+  {
+    id: "#1",
+    title : "Hello World!",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+  {
+    id: "#2",
+    title : "Binnacle",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+  {
+    id: "#3",
+    title : "My first app",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+  {
+    id: "#4",
+    title : "My first Website",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+  {
+    id: "#5",
+    title : "Taking some practice",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+  {
+    id: "#6",
+    title : "Practice, practice, practice",
+    description : "lorem...",
+    imagen: {
+      top: "",
+      botton: "",
+    },
+    title2: "Programs",
+    programs: "HTML5, CSS3, JS, jQuery, Boostrap, Angular",
+    imgDemo: ""
+  },
+ ];
+
+
+
 });
