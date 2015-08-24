834039a7026bd6a4684a7568354156581b84b911
dfm212 <daniel_ve14@hotmail.com>
2015-08-24 12:47:19 -0430

    debug link directives

--- js/angular-directives.js
+++ js/angular-directives.js
@@ -5,10 +5,10 @@
       return {
         restrict: 'AEC',
         // =====> G I T H U B
-        // templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/template-horizontal.html"
+        templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/template-horizontal.html"
 
         // =====> P R O D U C T I O N
-        templateUrl: "http://localhost/portfolio/HTML-templates/template-horizontal.html"
+        // templateUrl: "http://localhost/portfolio/HTML-templates/template-horizontal.html"
 
       };
     });
