d9b23588d082ce4c09389beecb9137f70e2f536b
dfm212 <daniel_ve14@hotmail.com>
2015-08-25 14:24:26 -0430

    act img demo

--- js/angular-directives.js
+++ js/angular-directives.js
@@ -9,6 +9,20 @@
 
         // =====> P R O D U C T I O N
         // templateUrl: "http://localhost/portfolio/HTML-templates/template-horizontal.html"
-
       };
     });
+
+      // ======================================================================
+      //      This directive is disable because the pluging for the scroll
+      // ======================================================================
+
+      // app.directive("demos", function() {
+        // return {
+          // restrict: 'AEC',
+          // =====> G I T H U B
+          // templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/template-horizontal.html"
+
+          // =====> P R O D U C T I O N
+          // templateUrl: "http://localhost/portfolio/HTML-templates/template-demos.html"
+        // };
+      // });
