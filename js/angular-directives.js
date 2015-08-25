6c74353ff8da1620e5a5699b0d274380698af4f0
dfm212 <daniel_ve14@hotmail.com>
2015-08-25 16:51:38 -0430

    Act final design
    this is the last act for design
    starting from this point everything will be Data and coding

--- js/angular-directives.js
+++ js/angular-directives.js
@@ -1,6 +1,17 @@
 
   var app = angular.module('portfolio-directives', []);
 
+  app.directive("menu", function() {
+    return {
+      restrict: 'AEC',
+      // =====> G I T H U B
+      templateUrl: "http://dfm212.github.io/portfolio/HTML-templates\menu-right.html"
+
+      // =====> P R O D U C T I O N
+      // templateUrl: "http://localhost/portfolio/HTML-templates/menu-right.html"
+    };
+  });
+
     app.directive("boards", function() {
       return {
         restrict: 'AEC',
