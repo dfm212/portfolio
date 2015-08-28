5b217bf9b2df847df16c41cba02a04fe6b1d0921
dfm212 <daniel_ve14@hotmail.com>
2015-08-28 01:56:56 -0430

    debug

--- js/angular-directives.js
+++ js/angular-directives.js
@@ -5,7 +5,7 @@
     return {
       restrict: 'E',
       // =====> G I T H U B
-      templateUrl: "http://dfm212.github.io/portfolio/HTML-templates\menu-right.html"
+      templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/menu-right.html"
 
       // =====> P R O D U C T I O N
       // templateUrl: "http://localhost/portfolio/HTML-templates/menu-right.html"
