2c80ce92a7f02672a92d3a557aedce21612f32be
dfm212 <daniel_ve14@hotmail.com>
2015-08-28 01:55:29 -0430

    debug

--- js/angular-directives.js
+++ js/angular-directives.js
@@ -49,7 +49,7 @@
         return {
           restrict: 'E',
           // =====> G I T H U B
-          templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/template-horizontal.html"
+          templateUrl: "http://dfm212.github.io/portfolio/HTML-templates/template-demos.html"
 
           // =====> P R O D U C T I O N
           // templateUrl: "http://localhost/portfolio/HTML-templates/template-demos.html"
